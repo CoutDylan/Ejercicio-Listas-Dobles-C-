c31b31ab4c086f2b4529401e400d166250b68267
CoutDylan <72243034+CoutDylan@users.noreply.github.com>
2020-11-07 09:24:45 -0500

    Modificaciones
    Continuacion funcion insertar listas B, coreccion de menu

--- Ejerciciolistas.h
+++ Ejerciciolistas.h
@@ -7,6 +7,7 @@ struct Nodo{ //struct Nodo
 	
 };	
 void insertarListaA(Nodo*&,int);
+void insertarListaB(Nodo*&,int);
 void MostrarListas(Nodo *);
 void menu(Nodo*punta){
 
@@ -14,9 +15,9 @@ void menu(Nodo*punta){
 	do{
 	cout<<endl;
 	cout<<"************************MENU**********************"<<endl;
-	cout<<"1. Insertar elemento"<<endl; 
-    cout<<"2. Mostrar elementos de la lista                  "<<endl;
-	cout<<"3. "<<endl;
+	cout<<"1. Crear Lista A (Paso 1)"<<endl; 
+    cout<<"2. Mostrar elementos de la lista(Paso 2)          "<<endl;
+	cout<<"3. Crear Lista B (Paso 3)  "<<endl;
 	cout<<"4. "<<endl;
 	cout<<"5.Salir "<<endl;
 	cout<<"***************************************************"<<endl;
@@ -35,8 +36,12 @@ void menu(Nodo*punta){
 	    break;
 	    case 2:MostrarListas(punta);
 	    break;
-	    
-	    case 3: 
+	    case 3: cout<<"|||INSERTOR DE LISTAS|||"<<endl;	 
+	    insertarListaB(punta,n);
+	    cout<<"*************************"<<endl;
+	    i++;
+	    system("pause");
+	    system("cls");
 	    
 	    break;
 	    
@@ -100,18 +105,93 @@ void MostrarListas(Nodo *punta){
 	system("cls");
 };
 
-void insertarListaB(Nodo *&punta, int p, int x){
+void insertarListaB(Nodo *&punta, int p){
+	int i,x,n;
+    Nodo *q;
+ 	Nodo* Nuevo=new Nodo();
+	char opcion;
+	int nnodos2;
+	Nuevo->dato=p;
+	cout<<"Cuantos nodos desea insertar en la lista B ( interna ) :"<<endl;
+    cin>>nnodos2;
+    while(i<nnodos2)
+{
+cout<<"HOLA 2"<<endl;
+Nodo *nuevo_nodo=new Nodo();
+Nodo *aux1=punta;
+nuevo_nodo->dato=n;
+
+if(punta==NULL){
+	punta=nuevo_nodo;
+	
+}
+else{
+	while(aux1->liga!=NULL){
+		aux1=aux1 ->liga;
+		
+	}
+	aux1->liga=nuevo_nodo;
+
+}
+i++;
+}
+	if(punta==NULL){
+		cout<<"Lista vacia, desea crear una nueva lista? (S/N): ";
+		cin>>opcion;
+		if (opcion == 'S'){
+			Nuevo->liga=NULL;
+			punta=Nuevo;
+			
+		}
+		}
+		else{
+			q=punta;
+			i=1;
+		}
+		    cout<<"Digite el nodo de la lista A en el que desea insertar la lista B (lista interna):"<<endl;
+			cin>>p;
+			while(q->liga != NULL){
+				
+				if(p==i){
+					Nuevo->liga=q->liga;
+					q->liga=Nuevo;	
+				}
+				else{
+					i++;
+					q=q->liga;
+				}
+				
+				if(q->liga==NULL){
+					cout<<"No existe esta posicion, desea insertarla de ultima (S/N)";
+					cin>>opcion;
+						if (opcion=='S'){
+							Nuevo->liga=NULL;
+							q->liga=Nuevo;
+						}
+				}
+				return;
+				}
+				
+			
+			
+			
+		
+		}
+	
+	/*
 	cout<<"Digite el nodo de la lista A en el que desea insertar la lista B (lista interna):"<<endl;
 	cin>>p;
+	Nodo *nuevo_nodo=new Nodo();
 	Nodo *x=punta;
 	x=punta;
 	while(x->liga!=p){
 		x=x->liga;
-}
+x->liga=nuevo_nodo;
+*/
 struct Nodo * crearpunta2(){
 struct Nodo * punta2;
-punta=NULL;
+punta2=NULL;
 return punta2;
 }
-}
+
 
